9b4954caeec30da2654318f34e70826f09e06cd7
Natalya Shipova <nataliefrance@mail.ru>
2023-10-12 09:21:49 +0300

    В класс Animal добавлено поле hasFur, гетеры и сетеры

--- src/Ex_7/Animal.java
+++ src/Ex_7/Animal.java
@@ -2,25 +2,53 @@ package Ex_7;
 
 public abstract class Animal {
 
+    private int quantityOfLegs;
     int age;
     protected int weight;
-    public int quantityOfLegs;
+    public boolean hasFur;
 
-    public Animal(){}
+    public Animal(){
+    }
 
-    public Animal(int age, int weight) {
+    public Animal(int quantityOfLegs, int age, int weight, boolean hasFur) {
+        this.quantityOfLegs = quantityOfLegs;
         this.age = age;
         this.weight = weight;
+        this.hasFur = hasFur;
     }
     private void eat() {
         System.out.println("Животное поело.");
     }
 
     void pee() {
-        System.out.println("Животное сходило в кусты");
+        System.out.println("Животное сходило в кусты.");
     }
 
     protected void sleep() {
-        System.out.println("Животное уснуло");
+        System.out.println("Животное уснуло.");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getQuantityOfLegs() {
+        return quantityOfLegs;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setQuantityOfLegs(int quantityOfLegs) {
+        this.quantityOfLegs = quantityOfLegs;
     }
 }
