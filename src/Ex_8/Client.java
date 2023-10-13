8a3cebb0e999c820ce50e64dcaf4d0d5b361aace
Natalya Shipova <nataliefrance@mail.ru>
2023-10-13 11:08:40 +0300

    Создан класс Main; Прописаны поля классам Person, Client, Employee.

--- src/Ex_8/Client.java
+++ src/Ex_8/Client.java
@@ -1,4 +1,32 @@
 package Ex_8;
 
 public class Client extends Person implements Printable, SeasonHandler {
+
+    private String request;
+    private boolean isAlreadyPaid;
+    private boolean isSatisfied;
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public void setAlreadyPaid(boolean alreadyPaid) {
+        isAlreadyPaid = alreadyPaid;
+    }
+
+    public void setSatisfied(boolean satisfied) {
+        isSatisfied = satisfied;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public boolean isAlreadyPaid() {
+        return isAlreadyPaid;
+    }
+
+    public boolean isSatisfied() {
+        return isSatisfied;
+    }
 }
--- src/Ex_8/Employee.java
+++ src/Ex_8/Employee.java
@@ -1,4 +1,22 @@
 package Ex_8;
 
 public class Employee extends Person implements Printable, SeasonHandler {
+    private int salary;
+    private boolean isDidJob;
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDidJob(boolean didJob) {
+        isDidJob = didJob;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public boolean isDidJob() {
+        return isDidJob;
+    }
 }
