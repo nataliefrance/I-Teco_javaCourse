b3cd663af12aec954b6f80b2dfb3885b8196eb30
Natalya Shipova <nataliefrance@mail.ru>
2023-10-13 11:01:34 +0300

    Написаны перечисления Season

--- src/Ex_8/Season.java
+++ src/Ex_8/Season.java
@@ -1,5 +1,14 @@
 package Ex_8;
 
 public enum Season {
+    WINTER("white"),
+    SPRING("blue"),
+    SUMMER("green"),
+    AUTUMN("yellow");
 
+    private String color;
+
+    Season (String color) {
+        this.color = color;
+    }
 }
