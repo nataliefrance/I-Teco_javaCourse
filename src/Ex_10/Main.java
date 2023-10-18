8c7994b39295bb62d5e719a34dde74bf8f3f81c9
Natalya Shipova <nataliefrance@mail.ru>
2023-10-18 10:06:59 +0300

    Практическое задание №10 - Exception
    Signed-off-by: Natalya Shipova <nataliefrance@mail.ru>

---
+++ src/Ex_10/Main.java
@@ -0,0 +1,38 @@
+package Ex_10;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+            try {
+                if (isStringCorrect(s)) {
+                    System.out.println("Строка введена правильно! Молодец!");
+                    break;
+                }
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static boolean isStringCorrect(String s) throws MyException {
+        int digit;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                digit = Character.getNumericValue(ch);
+                throw new MyException("Строка содержит цифру " + digit);
+            }
+        }
+
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length - 2; i++) {
+            if (charArray[i] == charArray[i + 1] && charArray[i + 2] == charArray[i + 1]) {
+                throw new MyException("Эта буква повторяется 3 раза подряд " + charArray[i]);
+            }
+        }
+        return true;
+    }
+}
---
+++ src/Ex_10/MyException.java
@@ -0,0 +1,7 @@
+package Ex_10;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
