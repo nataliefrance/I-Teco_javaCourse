e1f39f2cc2fe03c47294305d5c5d4f9201917da2
Natalya Shipova <nataliefrance@mail.ru>
2023-10-30 13:16:25 +0300

    Переменные suitableAge и birthYear вынесла в константы, добавила тернарный оператор.
    Signed-off-by: Natalya Shipova <nataliefrance@mail.ru>

--- src/Ex_12/Main.java
+++ src/Ex_12/Main.java
@@ -1,31 +1,31 @@
 package Ex_12;
 
 public class Main {
+
+    public static final Condition<Integer> SUITABLE_AGE = age -> (age >= 17) && (age <= 40);
+    public static final Condition<Integer> BIRTH_YEAR = year -> year < 1995;
+
     public static void main(String[] args) {
-        Condition<Integer> suitableAge = age -> (age >= 17) && (age <= 40);
-        Condition<Integer> birthYear = year -> year < 1995;
 
         Person personGerbert = new Person("Frenk Gerbert", 30, 1800);
-        Student studentGerbert = convertPersonToStudent(personGerbert, suitableAge, birthYear);
+        Student studentGerbert = convertPersonToStudent(personGerbert);
 
         System.out.println(studentGerbert);
     }
 
-    private static Student convertPersonToStudent(Person person, Condition<Integer> isSuitableAge, Condition<Integer> isYearLess1995){
-        Student student = new Student();
-
+    private static Student convertPersonToStudent(Person person) {
         Converter<Person, Student> converter =
                 p -> new Student(p.getName(), p.getAge());
 
-        if (isSuitableAge.check(person.getAge())) {
-            student = converter.convert(person);
+        if (SUITABLE_AGE.check(person.getAge())) {
+            Student student = converter.convert(person);
 
-            if (isYearLess1995.check(person.getBirthYear())) {
-                student.setGroup("УбИН-01-22");
-            } else student.setGroup("УбИН-02-22");
-
-        } else System.out.println("Возраст не подходящий.");
-
-        return student;
+            student.setGroup(BIRTH_YEAR.check(person.getBirthYear())
+                    ? "УбИН-01-22"
+                    : "УбИН-02-22");
+        } else {
+            System.out.println("Возраст не подходящий.");
+        }
+        return new Student();
     }
 }
