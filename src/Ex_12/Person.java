e424a426b82b594eed033534e63a68d5b06543aa
Natalya Shipova <nataliefrance@mail.ru>
2023-10-30 10:29:19 +0300

    Созданы классы Student и Person.
    Signed-off-by: Natalya Shipova <nataliefrance@mail.ru>

---
+++ src/Ex_12/Person.java
@@ -0,0 +1,7 @@
+package Ex_12;
+
+public class Person {
+    private String name;
+    private int age;
+    private int birthYear;
+}
---
+++ src/Ex_12/Student.java
@@ -0,0 +1,7 @@
+package Ex_12;
+
+public class Student {
+    private String name;
+    private int age;
+    private String group;
+}
